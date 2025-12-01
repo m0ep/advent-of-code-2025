5ff8d04bc610358fc901839032a924ea1e9623ae
Florian M. <m0ep@users.noreply.github.com>
2025-12-01 21:48:30 +0100

    solved part 1 and 2

---
+++ main.py
@@ -0,0 +1,18 @@
+import importlib
+import sys
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(sys.argv, len(sys.argv))
+    if 1 == len(sys.argv):
+        now = datetime.now()
+        runDay = now.day
+    else:
+        runDay = int(sys.argv[1])
+
+    name = f'day{runDay:02d}'
+    day = importlib.import_module(name + '.main')
+    day.run(name)
+
+
+
